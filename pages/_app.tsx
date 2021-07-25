c6fdaf6c80a93b5cc38e156dd6efdec165716317
Lars Volkers <larsvolkers@hotmail.nl>
2021-07-25 10:55:59 +0200

    Block indexing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import React from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="robots" content="noindex" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
